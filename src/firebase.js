ec682caa772a1b202f67f963e7f5966a4ccac889
Zakaria <z.bouali.b@gmail.com>
2026-04-20 23:29:25 +0100

    fixed firebase and packages

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAbY8LIZ8J6WZdGkKrFmQPIxXdxI_YgiHU",
-  authDomain: "fnbv2-8998e.firebaseapp.com",
-  projectId: "fnbv2-8998e",
-  storageBucket: "fnbv2-8998e.firebasestorage.app",
-  messagingSenderId: "724931743793",
-  appId: "1:724931743793:web:e94831028b91893a628510"
+    apiKey: "AIzaSyAbY8LIZ8J6WZdGkKrFmQPIxXdxI_YgiHU",
+    authDomain: "fnbv2-8998e.firebaseapp.com",
+    projectId: "fnbv2-8998e",
+    storageBucket: "fnbv2-8998e.firebasestorage.app",
+    messagingSenderId: "724931743793",
+    appId: "1:724931743793:web:e94831028b91893a628510"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export services so we can use them in our pages
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export default app;
